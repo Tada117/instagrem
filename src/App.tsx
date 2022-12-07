bb562379958ee6aedc225ed5df3a47714729fcf2
Tu Nguyen <ntat.dev@gmail.com>
2022-12-07 23:22:06 +0700

    feat: add theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
 import Post from "components/Post";
 import ThemeContextWrapper from "context/ThemeContextWrapper";
+import Header from "layouts/Header";
 
 import Navigation from "layouts/Navigation";
 import HomePage from "pages/Home";
 
-
 function App() {
   return (
     <ThemeContextWrapper>
-
-      <div className="min-h-screen bg-light-100 p-24 pl-32 dark:bg-dark-800">
-        <Navigation />
-        <HomePage />
-
+      <div className="min-h-screen bg-light-100 dark:bg-dark-800">
+        <Header />
+        <Post />
       </div>
     </ThemeContextWrapper>
   );
